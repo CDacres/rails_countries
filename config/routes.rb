e2403428f7b1843f3aeebfee8f1dde5d7e844b75
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-10-02 20:41:05 +0100

    created restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  get 'countries/index'
 
-  get 'countries/show'
+	resources :countries
 
-  get 'countries/edit'
-
-  get 'countries/create'
-
-  get 'countries/new'
-
-  get 'countries/update'
-
-  get 'countries/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
